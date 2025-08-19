6b5667d2895bd367aae1a7a24c84d75a6724b27c
Theofili <theostefpol@gmail.com>
2025-08-19 12:33:56 +0300

    Add files via upload

---
+++ models/Localization.py
@@ -0,0 +1,85 @@
+
+
+
+#------------------------------------------------------------------------------------#
+          ## Prediction function for protein sequence location in cell ##   
+#------------------------------------------------------------------------------------#
+
+
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+# List your types and corresponding model directories - Only 2 trained models for now
+TYPES = [
+    "extracellular",
+    "reticulum",
+    "golgi",
+    "membrane", 
+    "mitochondria",
+    "nuclear",
+    "peroxisome",
+    "ribosome",
+]
+
+MODEL_DIR_TEMPLATE = ('C:/Users/theos/SubCel/SubCel/models/model_{}')
+
+# Load models and tokenizers
+
+models = {}
+tokenizers = {}
+
+
+for t in TYPES:
+    model_dir = MODEL_DIR_TEMPLATE.format(t)
+    models[t] = AutoModelForSequenceClassification.from_pretrained(model_dir)
+    tokenizers[t] = AutoTokenizer.from_pretrained(model_dir)
+
+def predict_localization(sequence):
+    sequence = sequence.upper()
+
+    # Check input size 
+    
+    if len(sequence) >= 64 and len(sequence) <= 1218:
+
+        scores = {}
+        for t in TYPES:
+            tokenizer = tokenizers[t]
+            model = models[t]
+            inputs = tokenizer(sequence, return_tensors='pt', padding=True, truncation=True)
+            if 'token_type_ids' in inputs:
+                del inputs['token_type_ids']
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            model = model.to(device)
+            inputs = {k: v.to(device) for k, v in inputs.items()}
+            with torch.no_grad():
+                outputs = model(**inputs)
+                logits = outputs.logits
+                prob = torch.softmax(logits, dim=1)[0, 1].item()  # probability for class 1
+                scores[t] = prob
+
+        # Get the type with highest probability and the highest probability the numeric value
+        ## Maybe if more than one has a prob > 0.5 it needs to print both?
+
+        predicted_prob = max(scores.values())
+        predicted_type = max(scores, key=scores.get)
+
+        
+        if  predicted_prob <= 0.5:
+            predicted_type = 'Not Matched'
+
+        return predicted_type, scores
+    
+    elif len(sequence) < 64 or len(sequence) > 1218: 
+        print('Invalid input: Sequence length is too short or too long')
+
+    elif not sequence:
+        print('Invalid input: Please provide a sequence')
+
+
+# Example usage:
+
+if __name__ == "__main__":
+    test_seq = "MAAETLLSSLLGLLLLGLLLPASLTGGVGSLNLEELSEMRYGIEILPLPVMGGQSQSSDVVIVSSKYKQRYECRLPAGAIHFQREREEETPAYQGPGIPELLSPMRDAPCLLKTKDWWTYEFCYGRHIQQYHMEDSEIKGEVLYLGYYQSAFDWDDETAKASKQHRLKRYHSQTYGNGSKCDLNGRPREAEVRFLCDEGAGISGDYIDRVDEPLSCSYVLTIRTPRLCPHPLLRPPPSAAPQAILCHPSLQPEEYMAYVQRQADSKQYGDKIIEELQDLGPQVWSETKSGVAPQKMAGASPTKDDSKDSDFWKMLNEPEDQAPGGEEVPAEEQDPSPEAADSASGAPNDFQNNVQVKVIRSPADLIRFIEELKGGTKKGKPNIGQEQPVDDAAEVPQREPEKERGDPERQREMEEEEDEDEDEDEDEDERQLLGEFEKELEGILLPSDRDRLRSEVKAGMERELENIIQETEKELDPDGLKKESERDRAMLALTSTLNKLIKRLEEKQSPELVKKHKKKRVVPKKPPPSPQPTGKIEIKIVRPWAEGTEEGARWLTDEDTRNLKEIFFNILVPGAEEAQKERQRQKELESNYRRVWGSPGGEGTGDLDEFDF"
+    pred_type, all_scores = predict_localization(test_seq)
+    print(f"Predicted type: {pred_type}")
+    print("All model scores:", all_scores) 
---
+++ models/Model_Golgi.py
@@ -0,0 +1,221 @@
+import pandas as pd
+
+
+def model_golgi(df_golgi):
+
+    
+
+    ## Import libraries
+
+    # Load basic modules
+    import os
+    import sys
+    import time
+    #import flash_attn   #cannont install and import for some reason
+    from os import path
+    import gc
+    import torch
+    import json
+    
+
+    # Load data modules
+    import numpy as np
+    import pandas as pd
+    from random import randrange
+    from progressbar import ProgressBar
+
+    # Load machine learning modules
+    #import triton
+    import transformers
+    from torch.utils.data import TensorDataset, DataLoader
+    from torch.distributed.fsdp.fully_sharded_data_parallel import FullOptimStateDictConfig, FullStateDictConfig
+    from transformers import AutoTokenizer, AutoModel, EarlyStoppingCallback, set_seed, BitsAndBytesConfig
+    from accelerate import FullyShardedDataParallelPlugin, Accelerator
+    from peft import (
+        LoraConfig,
+        get_peft_model,
+        get_peft_model_state_dict,
+        prepare_model_for_kbit_training,
+    )
+
+
+    model_name="RaphaelMourad/Mistral-Peptide-v1-15M"
+
+    # BnB configuration
+
+    print('Initialazing bnb configuration.../n')
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_compute_dtype=torch.bfloat16
+    )
+    #print(bnb_config)
+    print('Done!')
+
+    # Load FSPD configuration
+
+    print('Initialazing FSDP configuration.../n')
+    fsdp_plugin = FullyShardedDataParallelPlugin(
+        state_dict_config=FullStateDictConfig(offload_to_cpu=True, rank0_only=False),
+        optim_state_dict_config=FullOptimStateDictConfig(offload_to_cpu=True, rank0_only=False),
+    )
+    accelerator = Accelerator(fsdp_plugin=fsdp_plugin)
+    #print(fsdp_plugin, '/n')
+    print('Done!')
+
+
+    # Initialize Lora configuration
+
+    print('Initialazing Parameter-Efficient-Fine-Tuning.../n')
+    peft_config = LoraConfig(
+            r=16,
+            lora_alpha=16,
+            lora_dropout=0.5, #prevents overfitting
+            bias="none",
+            task_type="SEQ_CLS",
+            target_modules=["q_proj", "k_proj", "v_proj", "o_proj","gate_proj"]
+        )
+    #print(peft_config, '/n')
+    print('Done!')
+
+
+    # Initialize training arguments
+
+    print('Initialazing Training Arguments.../n')
+    training_args = transformers.TrainingArguments(
+        output_dir="./golgi_results",
+        eval_strategy="epoch",
+        save_strategy="epoch",
+        logging_steps=10,
+        learning_rate=2e-5,  # Adjusted learning rate for binary classification
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        num_train_epochs=20,
+        weight_decay=0.1,
+        bf16=False, #FALSE IN GPU PERFORMS BETTER???
+        report_to="none",
+        load_best_model_at_end = True,
+    )
+
+    import os
+    os.environ["WANDB_DISABLED"] = "true"
+
+    #print(training_args, '/n')
+    print('Done!')
+
+
+    # Load tokenizer
+
+    print('Load tokenizer.../n')
+    tokenizer_golgi=transformers.AutoTokenizer.from_pretrained(
+        model_name,
+        model_max_length=1000,
+        padding_side="left",
+        use_fast=True,
+        trust_remote_code=True,
+    )
+    tokenizer_golgi.eos_token = '[EOS]'
+    tokenizer_golgi.pad_token = '[PAD]'
+    #print(tokenizer, '/n')
+    print('Done!')
+
+    num_labels = 2
+
+    # Import the build in functions module
+
+    sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
+
+    from functions import SupervisedDataset, DataCollatorForSupervisedDataset, compute_metrics
+
+    from sklearn.model_selection import train_test_split
+
+    ## Defining all these so that I can use the Supervised dataset to pass the
+    # split data through a tokenizer
+
+    # Split the dataframe into training, validation, and test sets --- 80%/20%
+    train_df, temp_df = train_test_split(df_golgi, test_size=0.2, random_state=42)
+    val_df, test_df = train_test_split(temp_df, test_size=0.5, random_state=42)
+
+    # Make the paths -- NEED TO CHANGE THE FOLDER --
+    temp_train_path = "data/golgi_temp_train.csv"
+    temp_val_path = "data/golgi_temp_val.csv"
+    temp_test_path = "data/golgi_temp_test.csv"
+
+    # Save the dfs into csvs
+    train_df.to_csv(temp_train_path, index=False)
+    val_df.to_csv(temp_val_path, index=False)
+    test_df.to_csv(temp_test_path, index=False)
+
+
+    print(f"Training data saved to: {temp_train_path}")
+    print(f"Validation data saved to: {temp_val_path}")
+    print(f"Test data saved to: {temp_test_path}")
+
+    # Define datasets using the temporary file paths
+    train_dataset = SupervisedDataset(tokenizer=tokenizer_golgi, data_path=temp_train_path, kmer=-1)
+    val_dataset = SupervisedDataset(tokenizer=tokenizer_golgi, data_path=temp_val_path, kmer=-1)
+    test_dataset = SupervisedDataset(tokenizer=tokenizer_golgi, data_path=temp_test_path, kmer=-1)
+
+    data_collator = DataCollatorForSupervisedDataset(tokenizer=tokenizer_golgi)
+
+    print("Training dataset size:", len(train_dataset))
+    print("Validation dataset size:", len(val_dataset))
+    print("Test dataset size:", len(test_dataset))
+
+
+
+    # Load model
+
+    print('Model loading.../n')
+    model_01=transformers.AutoModelForSequenceClassification.from_pretrained(
+        model_name,
+        num_labels=num_labels,
+        output_hidden_states=False,
+        #quantization_config=bnb_config,
+        device_map='auto',
+        trust_remote_code=True,
+    )
+    model_01.config.pad_token_id = tokenizer_golgi.pad_token_id
+    #print(model, '/n')
+    print('Done!')
+    
+
+    # Start training 
+
+    print('Initialize trainer.../n')
+    trainer = transformers.Trainer(
+        model=model_01,  
+        args=training_args,
+        compute_metrics=compute_metrics,
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+        data_collator=data_collator,
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=3)]
+    )
+    print('Training started/n')
+    trainer.local_rank=training_args.local_rank
+    trainer.train()
+    print('Done')
+
+
+    results_path = training_args.output_dir+"/metrics"
+    results = trainer.evaluate(eval_dataset=test_dataset)
+    os.makedirs(results_path, exist_ok=True)
+    with open(os.path.join(results_path, "test_results.json"), "w") as f:
+        json.dump(results, f)
+
+    file_metric="results/metrics/test_results.json"
+    data_expe = pd.read_json(file_metric, typ='series')
+
+    model_dir = "C:/Users/theos/SubCel/SubCel/models/model_golgi"
+    model_01.save_pretrained(model_dir)
+    tokenizer_golgi.save_pretrained(model_dir)
+
+    ## Need to check if the metrics are good
+    return model_01, tokenizer_golgi, data_expe
+
+
+import pandas as pd
+df = pd.read_csv('C:/Users/theos/SubCel/SubCel/data/model_data/model_golgi_data.csv')
+model, tokenizer, metrics = model_golgi(df)
+
