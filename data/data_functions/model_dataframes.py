3f84523faf3bfbf62f0d45c69ed88ff19667c131
Theofili <theostefpol@gmail.com>
2025-08-19 13:09:48 +0300

    Update model_dataframes.py

--- data/data_functions/model_dataframes.py
+++ data/data_functions/model_dataframes.py
@@ -17,7 +17,7 @@ import pandas as pd
 ## Maybe this can be a function? 
 
 nuclear_df = GetData.get_data(db="protein",title='nuclear' , size=10000, output='nuclear')
-transmembrane_df = GetData.get_data(db="protein", title='transmembrane', size=10000, output='membrane')
+membrane_df = GetData.get_data(db="protein", title='membrane', size=10000, output='membrane')
 mitochondrial_df = GetData.get_data(db="protein", title='mitochondrial', size=10000, output='mitochondrial')
 extracellular_df = GetData.get_data(db="protein", title='extracellular', size=10000, output='extracellular') 
 golgi_df = GetData.get_data(db="protein", title='golgi', size=10000, output='golgi')
@@ -119,6 +119,7 @@ model_reticulum_df = make_binary_type_df(filtered_df, target_type='reticulum', s
 model_ribosome_df = make_binary_type_df(filtered_df, target_type='ribosome', sample_n=200, out_csv='model_ribosome_data.csv')
 model_lysosome_df = make_binary_type_df(filtered_df, target_type='lysosome', sample_n=5000, out_csv='model_lysosome_data.csv')
 model_peroxisome_df = make_binary_type_df(filtered_df, target_type='peroxisome', sample_n=300, out_csv='model_peroxisome_data.csv')
+
 
 
 
