a4bef53bc2dfcf78b915685c444b91b6195128b3
Theofili <theostefpol@gmail.com>
2025-08-20 09:40:52 +0300

    Create define_directory.py

---
+++ define_directory.py
@@ -0,0 +1,9 @@
+
+
+#---------------------------------------------------------#
+       #User input for primary project file path
+#---------------------------------------------------------#
+
+def directory():
+    PROJECT_DIR = input('Please enter primary directory for code: ')
+    return(PROJECT_DIR)
