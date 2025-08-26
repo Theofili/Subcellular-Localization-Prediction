640db67c93b74480919c20e471f717dd12db6dfa
Theofili <theostefpol@gmail.com>
2025-08-26 11:53:13 +0300

    Update Model_Mitochondria.py

--- models/Model_Mitochondria.py
+++ models/Model_Mitochondria.py
@@ -4,8 +4,14 @@ import pandas as pd
 def model_mitochondria(df_mitochondria):
 
     
-
     ## Import libraries
+
+    import sys 
+
+    if sys.argv[1].lower() == 'true':
+        acc = True
+    else:
+        acc = False
 
     # Load basic modules
     import os
@@ -220,6 +226,7 @@ def model_mitochondria(df_mitochondria):
 import pandas as pd
 df = pd.read_csv('data/model_data/model_mitochondria_data.csv')
 model, tokenizer, metrics = model_mitochondria(df)
+
 
 
 
