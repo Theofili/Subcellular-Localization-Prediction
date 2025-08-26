6b1af424b2ca09fe5db1a79b8ee7bb759c37158d
Theofili <theostefpol@gmail.com>
2025-08-26 11:53:48 +0300

    Update Model_Peroxisome.py

--- models/Model_Peroxisome.py
+++ models/Model_Peroxisome.py
@@ -3,9 +3,14 @@ import pandas as pd
 
 def model_peroxisome(df_peroxisome):
 
-    
-
     ## Import libraries
+
+    import sys 
+
+    if sys.argv[1].lower() == 'true':
+        acc = True
+    else:
+        acc = False
 
     # Load basic modules
     import os
@@ -220,6 +225,7 @@ def model_peroxisome(df_peroxisome):
 import pandas as pd
 df = pd.read_csv('data/model_data/model_peroxisome_data.csv')
 model, tokenizer, metrics = model_peroxisome(df)
+
 
 
 
