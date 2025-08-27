fe77ce545290ba6f100fdb06058b9bbc00f26e2a
Theofili <theostefpol@gmail.com>
2025-08-27 09:24:10 +0300

    Delete data/data_functions/define_directory.py

--- data/data_functions/define_directory.py
+++
@@ -1,9 +0,0 @@
-
-
-#---------------------------------------------------------#
-       #User input for primary project file path
-#---------------------------------------------------------#
-
-def directory():
-    PROJECT_DIR = input('Please enter primary directory for code: ')
-    return(PROJECT_DIR)
